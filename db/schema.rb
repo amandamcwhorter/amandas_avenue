620d2176f1f7799750d7e01e990994578e2e8109
amandamcwhorter <amanda.mcwhorter@gmail.com>
2014-06-19 10:08:12 -0700

    changed compile in prod to true

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140514053721) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "homes", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
